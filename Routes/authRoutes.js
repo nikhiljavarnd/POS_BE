c0730c79be56defb2f9c8196d15c6a66cbe2ae75
nikhiljavarnd <nikhil.javarndcorp@gmail.com>
2021-12-23 10:12:16 -0500

    Changed directory path for server

--- Routes/authRoutes.js
+++ Routes/authRoutes.js
@@ -1,16 +1,12 @@
-const { Router } = require('express');
-const express = require('express');
+const { Router } = require("express");
+const express = require("express");
 
-const authController = require('../controllers/authController');
-
+const authController = require("../controllers/authController");
 
 const router = express.Router();
 
-router.post('/signup',authController.signup);
+router.post("/signup", authController.signup);
 
-router.post('/login',authController.login);
+router.post("/login", authController.login);
 
 module.exports = router;
-
-
-
